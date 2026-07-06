577005bf759d059e87e5ade2fe1bb4f43798973b
Tawasoulmohamedkhair <tawasoulmohamedkhair@gmail.com>
2026-07-06 21:20:59 +0400

    update routes and home screen

--- lib/core/routes/routes.dart
+++ lib/core/routes/routes.dart
@@ -1,15 +1,12 @@
 import 'package:go_router/go_router.dart';
 import 'package:masrofy/core/routes/app_routes.dart';
+import 'package:masrofy/features/auth/view/screens/auth_screen.dart';
 import 'package:masrofy/home_screen.dart';
-import 'package:masrofy/settings_screen.dart';
 
 final GoRouter appRouter = GoRouter(
-  initialLocation: AppRoutes.home,
+  initialLocation: AppRoutes.auth,
   routes: [
+    GoRoute(path: AppRoutes.auth, builder: (context, state) => AuthScreen()),
     GoRoute(path: AppRoutes.home, builder: (context, state) => HomeScreen()),
-    GoRoute(
-      path: AppRoutes.settings,
-      builder: (context, state) => SettingsScreen(),
-    ),
   ],
 );
